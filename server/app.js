5b2e6e323472fa79da1d53b45e4a47c4543de9b7
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-09-19 14:28:18 -0700

    fixed queries

--- server/app.js
+++ server/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 require("dotenv").config();
+const date = require('date-and-time')
 const client = require('./dbpg.js');
 const app = express();
 const port = process.env.PORT || 3000;
-
-//import dateFormat from 'dateformat';
-//const formattedDate = dateFormat(date, 'dddd, mmmm dS, yyyy');
 
 app.use(express.json());
 
@@ -27,30 +25,74 @@ group by az.id
 limit 5
 OFFSET 3`
 
-  let getQuery = `select az.*, COALESCE(json_agg(json_build_object('id', ap.id, 'url', ap.url))
+  let getQuery = `select json_build_object
+  (
+  'product_id', qs.product_id,
+  'results', ( select json_agg(json_build_object(
+         'question_id', qs.id,
+         'question_body', qs.body,
+         'question_date', qs.date_written,
+         'asker_name', qs.asker_name,
+         'question_helpfulness', qs.helpful,
+         'reported', qs.reported,
+         'answers', (select json_build_object(
+              az.id, json_build_object(
+                  'id', az.id,
+                  'body', az.body,
+                  'date', az.date_written,
+                  'answerer_name', az.answerer_name,
+                  'helpfulness', az.helpful,
+                  'reported', az.reported,
+                  'photos', (select COALESCE(json_agg(json_build_object('id', ap.id, 'url', ap.url))
+                                               FILTER (WHERE ap.id IS NOT NULL), '[]')
+                             from answers_photos ap where ap.answer_id=az.id)))
+                   from answers az
+                   where az.question_id=qs.id)
+              )
+          )
+      )
+  )
+  from questions qs
+  WHERE qs.product_id = 1
+  GROUP BY qs.product_id;`;
+
+    let getQuery2 = `select az.*, COALESCE(json_agg(json_build_object('id', ap.id, 'url', ap.url))
     FILTER (WHERE ap.id IS NOT NULL), '[]') photos
     from reviews az
     left join reviews_photos ap on ap.review_id=az.id where az.product_id = ${req.params.id}
     group by az.id`;
 
-    let getQuery2 = `select az.*, COALESCE(json_agg(json_build_object('id', ap.id, 'url', ap.url))
-    FILTER (WHERE ap.id IS NOT NULL), '[]') photos
-      from reviews az
-      left join reviews_photos ap on ap.review_id=az.id where az.id = 1
-      group by az.id
-      limit 5
-      OFFSET 3`;
+    let getQuery3 = `
+    SELECT json_build_object
+(
+	'product', ${req.params.id},
+	'page', 0,
+	'count', 5,
+	'results', (SELECT json_agg(json_build_object(
+		'review_id', rw.id,
+		'rating', rw.rating,
+		'summary', rw.summary,
+		'recommend', rw.recommend,
+		'response', rw.response,
+		'body', rw.body,
+		'date', rw.date,
+		'reviewer_name', rw.reviewer_name,
+		'helpfulness', rw.helpfulness,
+		'photos', (SELECT COALESCE(json_agg(json_build_object('id', rp.id, 'url', rp.url))
+				   FILTER (WHERE rp.id IS NOT NULL), '[]')
+				   FROM reviews_photos rp WHERE rp.review_id=rw.id)))
+	)
 
-  client.query(getQuery, (err, result)=>{
+
+)
+FROM reviews rw
+WHERE rw.product_id = ${req.params.id}
+GROUP BY rw.product_id;
+    `;
+
+  client.query(getQuery3, (err, result)=>{
       if(!err){
-        let data = {
-          products: parseInt(req.params.id) || 1,
-          page: parseInt(req.params.page) || 0,
-          count: parseInt(req.params.count) || 5,
-          results: result.rows
-        };
-
-          res.send(data);
+          res.send(result.rows[0].json_build_object);
       }
   });
   client.end;
@@ -64,36 +106,56 @@ app.get('/reviews/meta', (req, res)=>{
 
 app.get('/reviews/meta/:id', (req, res)=>{
 
-    let getQuery = `select az.*, COALESCE(json_agg(json_build_object('id', ap.id, 'url', ap.url))
-    FILTER (WHERE ap.id IS NOT NULL), '[]') photos
-    from reviews az
-    left join reviews_photos ap on ap.review_id=az.id where az.product_id = ${req.params.id}
-    group by az.id`;
+    let getQuery = `
+    SELECT json_build_object
+(
+'product_id', ${req.params.id},
+'ratings', (SELECT json_object_agg(rww.rating, (SELECT COUNT (rc.rating) FROM reviews as rc WHERE rc.product_id = ${req.params.id} GROUP BY rc.product_id))
+			 FROM reviews as rww
+			 WHERE rww.product_id = ${req.params.id}
+			 GROUP BY rww.product_id
+			),
+
+'recommended', (SELECT json_build_object('false', COUNT (rw.recommend::int), 'true', COUNT (rw.recommend::int))
+			FROM reviews rw
+			WHERE rw.product_id = ${req.params.id}
+			GROUP BY rw.product_id
+			   ),
+'characteristics', (SELECT json_object_agg (c.name, (select json_build_object('id', cr.id, 'value', (cr.value::float / cr.id))
+				  FROM characteristic_reviews as cr, reviews as rw
+				  WHERE cr.review_id = rw.id
+				  GROUP BY cr.id
+				  LIMIT 1
+													)
+										   )
+				   FROM characteristics as c
+				   WHERE c.product_id = rw.product_id
+				   GROUP BY c.product_id)
+)
+FROM reviews rw
+WHERE rw.product_id = ${req.params.id}
+GROUP BY rw.product_id`;
 
   client.query(getQuery, (err, result)=>{
     if(!err){
-      let data = {
-        products: parseInt(req.params.id) || 1,
-        results: result.rows
-      };
-        res.send(data);
+        res.send(result.rows[0].json_build_object);
     }
   });
   client.end;
 })
 
 app.post('/reviews', (req, res)=> {
-  const user = req.body;
-  let insertQuery = `insert into users(product_id, rating, summary, body, recommend, name, email, photos, characteristics)
-                      values(${user.product_id},
-                      '${user.rating}',
-                      '${user.summary}',
-                      '${user.body}'),
-                      '${user.recommend}'),
-                      '${user.name}'),
-                      '${user.email}'),
-                      '${user.photos}'),
-                      '${user.characteristics}')`
+  const review = req.body;
+  let insertQuery = `insert into reviews(product_id, rating, summary, body, recommend, name, email, photos, characteristics)
+                      values('${review.product_id}',
+                      '${review.rating}',
+                      '${review.summary}',
+                      '${review.body}'),
+                      '${review.recommend}'),
+                      '${review.name}'),
+                      '${review.email}'),
+                      '${review.photos}'),
+                      '${review.characteristics}')`
 
   client.query(insertQuery, (err, result)=>{
       if(!err){
@@ -105,12 +167,11 @@ app.post('/reviews', (req, res)=> {
 })
 
 app.put('/reviews/:id/helpful', (req, res)=> {
-  let review = req.body;
-  let updateQuery = `update users
-                    set firstname = '${review.firstname}',
-                    lastname = '${review.lastname}',
-                    location = '${review.location}'
-                    where id = ${review.id}`
+
+  let updateQuery = `update reviews
+  set helpfulness = helpfulness + 1
+  FROM reviews rw
+  WHERE reviews.id = ${req.params.id}`
 
   client.query(updateQuery, (err, result)=>{
       if(!err){
@@ -121,12 +182,16 @@ app.put('/reviews/:id/helpful', (req, res)=> {
   client.end;
 })
 
-app.delete('/reviews/:id', (req, res)=> {
-  let insertQuery = `delete from reviews where id=${req.params.id}`
+app.put('/reviews/:id/report', (req, res)=> {
+
+  let updateQuery = `update reviews
+  set reported = true
+  FROM reviews rw
+  WHERE reviews.id = ${req.params.id}`
 
   client.query(insertQuery, (err, result)=>{
       if(!err){
-          res.send('Deletion was successful')
+          res.send('Update was successful')
       }
       else{ console.log(err.message) }
   })
---
+++ server/stress_test.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 100 }, // below normal load
+    { duration: '20s', target: 100 },
+    { duration: '20s', target: 200 }, // normal load
+    { duration: '20s', target: 300 },
+    { duration: '20s', target: 400 }, // stress load
+    { duration: '20s', target: 400 },
+    { duration: '10s', target: 0 }, // scale down
+  ],
+};
+
+const API_URL = 'http://localhost:3000';
+
+
+
+export default () => {
+  http.batch([
+    ['GET', `${API_URL}/reviews/1`],
+    ['GET', `${API_URL}/reviews/meta/1`],
+  ]);
+  sleep(1);
+};
